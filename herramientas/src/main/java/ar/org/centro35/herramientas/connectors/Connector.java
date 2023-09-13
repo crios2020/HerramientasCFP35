13f51bc42fbdfdc19566f356e04e83f9a0c40c33
Carlos Rios <intercrios@hotmail.com>
2023-09-12 23:03:45 -0300

    Update Connector.java

--- herramientas/src/main/java/ar/org/centro35/herramientas/connectors/Connector.java
+++ herramientas/src/main/java/ar/org/centro35/herramientas/connectors/Connector.java
@@ -4,15 +4,10 @@ import java.sql.DriverManager;
 public class Connector {
 
     // Localhost   
-    private static String url="jdbc:mariadb://localhost:3306/herramientasCFP35";
-    private static String user="root";
-    private static String pass="";
-
-    // db4free
-    // private static String url="jdbc:mariadb://db4free.net:3306/basegeneral";
-    // private static String user="basegeneral";
-    // private static String pass="basegeneral";
-
+    private static String url="jdbc:mariadb://xxx:3306/xxx";
+    private static String user="xxx";
+    private static String pass="xxx";
+    
     private static Connection conn=null;
 
     public static String getUrl() {
