d4cca53d31a0aefcd995bbdc1b5593e7cb0c7a06
Carlos <intercrios@hotmail.com>
2023-10-17 15:32:06 -0300

    Mejoras IP

--- herramientas/src/main/resources/static/js/main.js
+++ herramientas/src/main/resources/static/js/main.js
@@ -1,16 +1,16 @@
 function ActualizarHora() {
-    let fecha = new Date();
-    let segundos = fecha.getSeconds();
-    let minutos = fecha.getMinutes();
-    let horas = fecha.getHours();
+    let fecha = new Date()
+    let segundos = fecha.getSeconds()
+    let minutos = fecha.getMinutes()
+    let horas = fecha.getHours()
 
-    let elementoHoras = document.getElementById("pHoras");
-    let elementoMinutos = document.getElementById("pMinutos");
-    let elementoSegundos = document.getElementById("pSegundos");
+    let elementoHoras = document.getElementById("pHoras")
+    let elementoMinutos = document.getElementById("pMinutos")
+    let elementoSegundos = document.getElementById("pSegundos")
 
-    elementoHoras.textContent = horas.toString().padStart(2, "0");
-    elementoMinutos.textContent = minutos.toString().padStart(2, "0");
-    elementoSegundos.textContent = segundos.toString().padStart(2, "0");
+    elementoHoras.textContent = horas.toString().padStart(2, "0")
+    elementoMinutos.textContent = minutos.toString().padStart(2, "0")
+    elementoSegundos.textContent = segundos.toString().padStart(2, "0")
 
     let hora=   horas.toString().padStart(2, "0")+":"+
                 minutos.toString().padStart(2, "0")+":"+
@@ -21,19 +21,27 @@ function ActualizarHora() {
             
 }
 
-setInterval(ActualizarHora, 1000);
+setInterval(ActualizarHora, 1000)
 
 
 function confirmarEliminacion() {
-    return window.confirm("¿Estás seguro de que deseas eliminar?");
+    return window.confirm("¿Estás seguro de que deseas eliminar?")
 }
 
 function confirmarDevolucion() {
-    return window.confirm("¿Estás seguro de que deseas devolver?");
+    return window.confirm("¿Estás seguro de que deseas devolver?")
 }
 
 function get_ip(obj){
     try{
-        document.getElementById('ip').setAttribute('value',obj.ip)
+        let arrayIP = obj.ip.split('.')
+        let stringIP = ''
+        for (var i = 0; i < arrayIP.length; i++) {
+            //console.log(arrayIP[i] + " / ")
+            stringIP=stringIP+arrayIP[i].padStart(3, "0")
+            if(i != arrayIP.length-1) stringIP=stringIP+"."
+        }
+        //console.log(stringIP)
+        document.getElementById('ip').setAttribute('value',stringIP)
     }catch(e){}  
 }
